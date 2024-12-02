9e18728a33de0fac4d5117c0eab6e089a641a2af
PrimeRin <primedruk312@gmail.com>
2024-12-02 21:11:04 +0600

    get all employee done

--- src/main/java/com/spring/demo/service/EmployeeService.java
+++ src/main/java/com/spring/demo/service/EmployeeService.java
@@ -2,8 +2,12 @@ package com.spring.demo.service;
 
 import com.spring.demo.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployee();
 }
